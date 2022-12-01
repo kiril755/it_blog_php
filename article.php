cc9f584baedd9463bd55617f3efa60b0d4f7f364
Minaiev Kiril <kiril777kiril@gmail.com>
2022-12-01 15:54:23 +0200

    add comment form

--- article.php
+++ article.php
@@ -74,6 +74,7 @@
                             </div>
                     </div>
                     <div class="block">
+                             <a href="#comment-add-form">Добавить свой</a>
                              <h3>Комментарии</h3>
                              <div class="block__content">
                                <div class="articles articles__vertical">
@@ -102,6 +103,62 @@
                                </div>
                              </div>
                </div>
+
+                    <div class="block" id="comment-add-form">
+                      <h3>Добавить комментарий</h3>
+                      <div class="block__content">
+                        <form class="form" method="POST" action="/article.php?id=<?php echo $art['id']?>#comment-add-form">
+                        <?php 
+                            if (isset($_POST['do_post'])) {
+                                $errors = array();
+
+                                if ($_POST['name'] == '') {
+                                    $errors[] = 'Введите имя!';
+                                };
+                                if ($_POST['nickname'] == '') {
+                                    $errors[] = 'Введите Ваш никнейм!';
+                                }
+                                if ($_POST['email'] == '') {
+                                    $errors[] = 'Введите email!';
+                                }
+                                if ($_POST['text'] == '') {
+                                    $errors[] = 'Введите текст комментария!';
+                                }
+                                if(empty($errors)) {
+                                    // добавить комментарий
+                                    mysqli_query($connection, "INSERT INTO `comment` (`author`, `nickname`, `email`, `text`, `article_id`) VALUE ('".$_POST['name']."', $_POST['nickname'], $_POST['email'], $_POST['text'], $_POST['text'])");
+                                    echo '<span style="color: green; front-weight: bold; margin-bottom: 10px; display: block;">комментарий успешно добавлен</span>';
+                                } else {
+                                    // выводит ошибку
+                                    echo '<span style="color: red; front-weight: bold; margin-bottom: 10px; display: block;">' . $errors['0'] .  '</span>';
+                                }
+                            }
+                        ?>
+                          <div class="form__group">
+                            <div class="row">
+                              <div class="col-md-4">
+                                <input type="text" class="form__control" name="name" placeholder="Имя" value="<?php echo $_POST['name']?>">
+                              </div>
+                              <div class="col-md-4">
+                                <input type="text" class="form__control" name="nickname" placeholder="Никнейм" value="<?php echo $_POST['nickname']?>">
+                              </div>
+                              <div class="col-md-4">
+                                <input type="text" class="form__control" name="email" placeholder="Email (не будет показан)" value="<?php echo $_POST['email']?>">
+                              </div>
+                            </div>
+                          </div>
+                          <div class="form__group">
+                            <textarea name="text" class="form__control" placeholder="Текст комментария ...">
+                                <?php echo $_POST['text']?>
+                            </textarea>
+                          </div>
+                          <div class="form__group">
+                            <input type="submit" class="form__control" name="do_post" value="Добавить комментарий">
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+
                   </section>
                   <section class="content__right col-md-4">
                     <?php include "includes/sidebar.php";?>
